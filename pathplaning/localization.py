ba45d99bb1526042a96115036fd6f5c863485596
hjalte01 <hjalte@bjoernstrup.net>
2024-10-25 15:24:10 +0200

    fredags SIR

--- pathplaning/localization.py
+++ pathplaning/localization.py
@@ -3,22 +3,21 @@ import numpy as np
 from numpy import random as rnd
 # from types import List, Tuple
 
-noise_dist = 0.1
-noise_angle = 0.1
+noise_dist = 150
+noise_angle = 0.2
 sigma_dist = 1/np.sqrt(2*np.pi*np.power(noise_dist, 2))
 sigma_angle = 1/np.sqrt(2*np.pi*np.power(noise_angle, 2))
 
 class ParticleFilter(object):
-    def __init__(self, initial_pos: Position, n: int, grid: Grid):
+    def __init__(self, n: int, grid: Grid):
         super(ParticleFilter, self).__init__()
         self.n = n
         self.grid = grid
-        self.initial_pos = initial_pos
         self.particles = np.ndarray((self.n, 3))
-        # TODO - test if uniform inital is better, since we don't know where we start so initial_pos.x is undefined
-        self.particles[:, 0] = rnd.normal(initial_pos.x, noise_dist, self.n)
-        self.particles[:, 1] = rnd.normal(initial_pos.y, noise_dist, self.n)
-        self.particles[:, 2] = rnd.normal(initial_pos.rad, noise_angle, self.n)
+
+        self.particles[:, 0] = rnd.uniform(0, len(grid) * grid.zone_size, self.n)
+        self.particles[:, 1] = rnd.uniform(0, len(grid) * grid.zone_size, self.n)
+        self.particles[:, 2] = rnd.uniform(0, 2 * np.pi, self.n)
         self.weights = np.array((self.n))
         self.weights.fill(1/self.n)
     
@@ -38,37 +37,48 @@ class ParticleFilter(object):
         self.particles[:, 1] += (delta)*np.sin(self.particles[:, 2]) 
         weights = np.ones((self.n, 1))
 
-        # Update step
-        for pose in poses:
-            dist = np.sqrt((pose.cx - self.particles[:, 0])**2
-                + (pose.cy - self.particles[:, 1])**2)
-            dist = np.array(dist).reshape(-1, 1).transpose()
+        # take a photo get markers
+        observed_markers = {
+            1: (450*9, 0*np.pi/180),
+            2: (450*9, 90*np.pi/180)
+        }
+        if not isinstance(observed_markers, type(None)):
+            # Update step 
+            for index, (id, (marker_delta, marker_rad)) in enumerate(observed_markers.items()):
+                marker = next((m for m in self.grid.markers if m.id == id), None)
+                # print(id, marker)
+                if marker is None:
+                    continue
+                # print(marker.id, marker.cx, marker.cy)
 
-            marker_vec = np.array((pose.cx - self.particles[:, 0], pose.cy - self.particles[:, 1]))/dist # el
-            orientation_vec = np.array((np.cos(self.particles[:, 2]), np.sin(self.particles[:, 2]))) # et
-            hat_vec = np.array((-np.sin(self.particles[:, 2]), np.cos(self.particles[:, 2]))) # eth
-            
-            
-            
-            temp1 = np.sum(marker_vec * hat_vec, axis=0).reshape(-1, 1) # el * eth 
-            temp2 = np.sum(marker_vec * orientation_vec, axis=0).reshape(-1, 1)  #, el * et
-            angle = np.sign(temp1) * np.arccos(temp2) # el * eth , el * et
-            angle = np.array(angle)
+                dist = np.sqrt((marker.cx - self.particles[:, 0])**2
+                    + (marker.cy - self.particles[:, 1])**2)
+                dist = np.array(dist).reshape(-1, 1).transpose()
 
+                marker_vec = np.array((marker.cx - self.particles[:, 0], marker.cy - self.particles[:, 1]))/dist # el
+                orientation_vec = np.array((np.cos(self.particles[:, 2]), np.sin(self.particles[:, 2]))) # et
+                hat_vec = np.array((-np.sin(self.particles[:, 2]), np.cos(self.particles[:, 2]))) # eth
+                
+                temp1 = np.sum(marker_vec * hat_vec, axis=0).reshape(-1, 1) # el * eth 
+                temp2 = np.sum(marker_vec * orientation_vec, axis=0).reshape(-1, 1)  #, el * et
+                angle = np.sign(temp1) * np.arccos(temp2) # el * eth , el * et
+                angle = np.array(angle)            
+                dist = dist.transpose()
+                
+                weights *= sigma_dist * np.exp(-(marker_delta - dist)**2/((2*noise_dist)**2)) # pose.delta - 
+                weights *= sigma_angle*np.exp(-(marker_rad - angle)**2/((2*noise_angle)**2)) # pose.rad -
             
+            print(min(weights), max(weights))
             
-            dist = dist.transpose()
+            weights += 1.e-300 
+            weights /= sum(weights)
             
-            weights *= sigma_dist * np.exp(-((dist*0.99) - dist)**2/((2*noise_dist)**2)) # pose.delta - 
-            weights *= sigma_angle*np.exp(-((angle * 0.99) - angle)**2/((2*noise_angle)**2)) # pose.rad -
-
-        weights += 1.e-300 
-        weights /= sum(weights)
-        
-        # resample step
-        indexes = self.residual_resample(weights)
-        self.resample_from_index(indexes, weights)
-
+            # resample step
+            indexes = self.residual_resample(weights)
+            self.resample_from_index(indexes, weights)
+        else:
+            for i in len(weights):
+                weights[i] = 1/self.n
         # estimate step
         pos = np.average(self.particles[:, :2], weights=weights, axis=0)
         orientation = np.average(self.particles[:, 2], weights=weights)
@@ -104,18 +114,23 @@ class ParticleFilter(object):
 
     def run_pf(self):
 
-        for i in range(9):        
-            cell, orientation = self.update((450, 0), self.grid.markers)
+        for i in range(15):        
+            cell, orientation = self.update((450, 0.25*np.pi))
             print(cell, orientation*180/np.pi)
 
     
 
 def main():
 
-    grid = Grid((4, 4), 450)
-    grid.create_marker(grid[4, 1].diffuse(), grid[4, 1][4, 4], 1)
-    grid.create_marker(grid[2, 1].diffuse(), grid[2, 1][4, 4], 2)
-    pf = ParticleFilter(Position(1, 0), 100, grid)
+    grid = Grid((6, 1), 450)
+    grid.create_marker(grid[0, 0].diffuse(), grid[0, 0][0, 0], 1)
+    grid.create_marker(grid[0, 8].diffuse(), grid[0, 8][0, 0], 2)
+    grid.create_marker(grid[8, 0].diffuse(), grid[8, 0][0, 0], 3)
+    grid.create_marker(grid[8, 8].diffuse(), grid[8, 8][0, 0], 4)
+    # observered postition from particles to markers
+
+
+    pf = ParticleFilter(100000, grid)
     pf.run_pf()
 
 main()

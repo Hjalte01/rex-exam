f674aa5332512c4ee9c18eca0fa5660ef8b93a16
Elmegaard1901 <mathias.elmegaard@gmail.com>
2024-10-30 16:36:59 +0100

    new cali

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ LANDMARK_SIZE       = LANDMARK_SIZE # 200mm - The size of a landmark (box with m
 ZONE_SIZE           = ZONE_SIZE     # 450mm
 ZONES               = ZONES         # 9
 # Aruco settings
-MARKER_SIZE         = 74.04     # mm - The size of a marker on a landmark.
-BOARD_MARKER_SIZE   = 23.32     # mm - The size of a marker on a board.
-BOARD_SHAPE         = (5, 5)    # m x n
-BOARD_GAP           = 1.85      # mm
+MARKER_SIZE         = 92.12     # mm - The size of a marker on a landmark.
+BOARD_MARKER_SIZE   = 61.78     # mm - The size of a marker on a board.
+BOARD_SHAPE         = (3, 3)    # m x n
+BOARD_GAP           = 1.84      # mm
 ARUCO_DICT          = aruco.Dictionary_get(aruco.DICT_6X6_250)
 # Calibrate settings
 PASSES              = 12

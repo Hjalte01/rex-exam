d3d1d1464bf9436070c7fc99253a0b3cb72eb68f
Tim R. Jensen <timrjensen@gmail.com>
2024-11-01 14:05:17 +0100

    FEAT: 01-11-24 14:05

--- states/detect.py
+++ states/detect.py
@@ -59,8 +59,6 @@ class Detect(State):
             self.dist_coeffs
         )
 
-        first = None
-        last = None 
         for i, (rvec, tvec, id) in enumerate(zip(rvecs, tvecs, ids)):
             # all ids unique then go on else "contine" to the next iteration - only include the same marker id once 
             if not all(m.id != id for m in robot.grid.markers):

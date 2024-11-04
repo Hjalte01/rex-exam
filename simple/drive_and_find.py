0bd707ecfd65130e31e7a959d68e2c5e48616c1d
Elmegaard1901 <mathias.elmegaard@gmail.com>
2024-11-04 12:35:40 +0100

    camera out

--- simple/drive_and_find.py
+++ simple/drive_and_find.py
@@ -21,52 +21,52 @@ sys.path.append(parentdir)
 
 from robot import Robot
 
-try:
-    import picamera2
-    print("Camera.py: Using picamera2 module")
-except ImportError:
-    print("Camera.py: picamera2 module not available")
-    exit(-1)
+# try:
+#     import picamera2
+#     print("Camera.py: Using picamera2 module")
+# except ImportError:
+#     print("Camera.py: picamera2 module not available")
+#     exit(-1)
 
-# print("OpenCV version = " + cv2.__version__)
+# # print("OpenCV version = " + cv2.__version__)
 
-# Open a camera device for capturing
-imageSize = (1920, 1080)
-FPS = 30
-cam = picamera2.Picamera2()
-frame_duration_limit = int(1/FPS * 1000000) # Microseconds
-# Change configuration to set resolution, framerate
-picam2_config = cam.create_video_configuration({"size": imageSize, "format": 'RGB888'},
-                                                            controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
-                                                            queue=False)
-cam.configure(picam2_config) # Not really necessary
-cam.start(show_preview=False)
+# # Open a camera device for capturing
+# imageSize = (1920, 1080)
+# FPS = 30
+# cam = picamera2.Picamera2()
+# frame_duration_limit = int(1/FPS * 1000000) # Microseconds
+# # Change configuration to set resolution, framerate
+# picam2_config = cam.create_video_configuration({"size": imageSize, "format": 'RGB888'},
+#                                                             controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
+#                                                             queue=False)
+# cam.configure(picam2_config) # Not really necessary
+# cam.start(show_preview=False)
 
-time.sleep(1)  # wait for camera to setup
+# time.sleep(1)  # wait for camera to setup
 
     
-# Capture an image from the camera
-image = cam.capture_array("main")
-image_width = image.shape[1]
-image_height = image.shape[0]
+# # Capture an image from the camera
+# image = cam.capture_array("main")
+# image_width = image.shape[1]
+# image_height = image.shape[0]
 
-# Get the camera matrix and distortion coefficients
-cam_matrix = np.zeros((3, 3))
-coeff_vector = np.zeros(5)
+# # Get the camera matrix and distortion coefficients
+# cam_matrix = np.zeros((3, 3))
+# coeff_vector = np.zeros(5)
 
-focal_length = 1694.0
-principal_point = (image_width / 2, image_height / 2)
+# focal_length = 1694.0
+# principal_point = (image_width / 2, image_height / 2)
 
-cam_matrix[0, 0] = focal_length  # f_x
-cam_matrix[1, 1] = focal_length  # f_y
-cam_matrix[0, 2] = principal_point[0]  # c_x
-cam_matrix[1, 2] = principal_point[1]  # c_y
-cam_matrix[2, 2] = 1.0
+# cam_matrix[0, 0] = focal_length  # f_x
+# cam_matrix[1, 1] = focal_length  # f_y
+# cam_matrix[0, 2] = principal_point[0]  # c_x
+# cam_matrix[1, 2] = principal_point[1]  # c_y
+# cam_matrix[2, 2] = 1.0
 
-marker_length = 0.145 # meters
+# marker_length = 0.145 # meters
 
-# get the dictionary for the aruco markers
-img_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250) # As per the assignment
+# # get the dictionary for the aruco markers
+# img_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250) # As per the assignment
 
 
 def get_landmark(marker_id, cam, img_dict, cam_matrix, coeff_vector, marker_length):
